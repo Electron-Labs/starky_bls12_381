50cf17d9c54b382a84d897b170d2fe35874f082e
electron-team <utsavjn48@gmail.com>
2023-12-01 12:47:38 +0530

    setting up
    Signed-off-by: electron-team <utsavjn48@gmail.com>

---
+++ target/debug/deps/starky_bls12_381-7598988687a14e8f.d
@@ -0,0 +1,5 @@
+/Users/utsavjain/Desktop/electron_labs/starky_bls12_381/target/debug/deps/starky_bls12_381-7598988687a14e8f: src/main.rs
+
+/Users/utsavjain/Desktop/electron_labs/starky_bls12_381/target/debug/deps/starky_bls12_381-7598988687a14e8f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/starky_bls12_381.d
@@ -0,0 +1,1 @@
+/Users/utsavjain/Desktop/electron_labs/starky_bls12_381/target/debug/starky_bls12_381: /Users/utsavjain/Desktop/electron_labs/starky_bls12_381/src/main.rs
